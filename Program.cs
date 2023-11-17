a13310845edfb5d76000516d5aa2470eb786b58a
Andrei Hapaniuk <Andrey.Gapanyuk@100soft.by>
2023-11-17 11:36:47 +0300

    Timing, tuning

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.RegularExpressions;
 
 namespace LoadTestApp
 {
@@ -9,10 +10,13 @@ namespace LoadTestApp
     {
         private const string templateURL = "http://localhost:2323/api/search/{0}/100";
         private const string testdataFileName = "..\\..\\..\\testcases\\names.txt";
-        private const int clientsCount = 10;
+        private const int clientsCount = 15;
 
         static async Task Main(string[] args)
         {
+            int requests = 0;
+            var lckRequests = new object();
+
             DocumentsModel data = null;
             List<string> searchStrings = new List<string>();
 
@@ -27,7 +31,8 @@ namespace LoadTestApp
 
             var splittedTestData = searchStrings.ChunkBy(searchStrings.Count / clientsCount).ToList();
 
-            List<Task> tasks = new List<Task>(splittedTestData.Count);
+            List<Task> tasks = new List<Task>(splittedTestData.Count+1);
+
 
             int id = 0;
             foreach (var testdata in splittedTestData)
@@ -42,16 +47,22 @@ namespace LoadTestApp
                     {
                         using (HttpClient client = new HttpClient())
                         {
-                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            var searchUrl = string.Format(templateURL, searchString);
+                           client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                            var pattern = new Regex(@"[:!@#$%^&*()}{|\:?><\[\]\\;'/.,~_-]");                          
+                            var searchUrl = string.Format(templateURL, pattern.Replace(searchString, " "));
 
                             sw.Start();
-                            HttpResponseMessage response = await client.GetAsync(searchUrl).ConfigureAwait(false);
+                            HttpResponseMessage response = client.GetAsync(searchUrl).ConfigureAwait(false).GetAwaiter().GetResult();
+                            lock (lckRequests)
+                            {
+                                requests ++;
+                            }
                             sw.Stop();
 
                             if (response.IsSuccessStatusCode)
                             {
-                                data = await response.Content.ReadFromJsonAsync<DocumentsModel>().ConfigureAwait(false);
+                                data =  response.Content.ReadFromJsonAsync<DocumentsModel>().GetAwaiter().GetResult();
 
                                 int dataCount = (data != null && data.Documents.Any()) ? data.Documents.Count() : 0;
 
@@ -60,17 +71,30 @@ namespace LoadTestApp
                             }
                             else
                             {
-                                Console.WriteLine($"Client #{clientId}. Error {0} ({1}). Request [{searchString}] ",
-                                                  (int)response.StatusCode, response.ReasonPhrase);
+                                Console.WriteLine($"Client #{clientId}. Error {(int)response.StatusCode} ({response.ReasonPhrase}) SEARCH={searchString}");
                             }
 
                             sw.Reset();
                         }
 
-                        // await Task.Delay(100);
+                        //await Task.Delay(10);
                     };
                 }));
             }
+
+            await Task.Delay(15000);
+            
+            tasks.Add(Task.Run(async () => {
+                int oldRequests = 0;
+                while (true)
+                {
+                    ConsoleUtils.WriteStepInfo(3, $"Requests per second = {requests - oldRequests}");
+                    //Console.WriteLine($"Requests per second = {requests - oldRequests}");
+                    oldRequests = requests;
+                    await Task.Delay(1000).ConfigureAwait(false);
+                }
+
+            }));
 
             await Task.WhenAll(tasks);
 
